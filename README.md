133bc63fd34622d235d977fcd3abe2dcffa18555
gneisscode <103408439+gneisscode@users.noreply.github.com>
2023-02-19 13:03:08 +0200

    added script file and edited README

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# operators-control-flow
+#1 
+
+Arithmetic Operators : -, *, /, %, --, ++
+Assignment Operators : =, +=, -=, *=, /=, %=
+Comparison Operators : >, >=, <, <=, ===, !==
+Logical Operators : &&, ||, !
+Bitwise Operators: &, |, ^, ~, <<, >>, >>>
+
+#2 
+Arithmetic Operators: let b = x + y, for (let x= 1; x<10; x++){x+=1}
+Assignment Operators: x+=2, y%=4
+Comparison Operators: x>2, y<=6
+Logical Operators: x=2 || y=4, x=3 && y=8
+Bitwise Operators: ~5, 5<<1
+
+#4
+when the program is executed, i= 1 and after every loop , 7 is added to each instance of i as long as the condition of i being less than 20 is met. therefore the result of the for loop is; 
+1, 8, 15
---
+++ index.js
@@ -0,0 +1,56 @@
+ //Question 3
+
+let science = "Physics, Chemistry, Biology, Technical Drawing";
+let socscience = "Accounting, Commerce, Marketing, Geography";
+let arts = "Government, Economics, Literature, History";
+let general = "English, Mathematics";
+
+let bolatito = "arts"
+
+function subjectFinder(name) {
+    if (name==="art_student"){
+        console.log(arts)
+    }
+
+    else if (name === "socscience") {
+        console.log(socscience)
+    } 
+
+    else if (name === "science") {
+        console.log(science)
+    } 
+    
+    else {
+      console.log(general);
+    }
+}
+
+
+subjectFinder(bolatito)
+
+
+
+//Question 5
+
+
+function highestPowerof2(num)
+{
+	let pwr = null;
+	for (let i = num; i >= 1; i--)
+		{
+		if ((i & (i - 1)) == 0)
+			{
+				pwr = i;
+				break;
+			}
+		}
+	console.log(`The number ${pwr} is the power of 2 nearest to ${num}`);
+}
+
+
+
+highestPowerof2(40)
+
+
+
+
